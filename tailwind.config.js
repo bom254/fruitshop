45417b3eb36df64e501348282f509ceb62d4ba47
bom254 <bevonmokuas@gmail.com>
2025-02-06 17:56:58 +0000

    added compenents

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#de2c4d",
+                secondary: "#fb923c",
+            },
+
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+                averia: ["Averia Serif Libre", "serif"]
+            },
+
+            container: {
+                center: true,
+            }
+        },
     },
     plugins: [],
 }
